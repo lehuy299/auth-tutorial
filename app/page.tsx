f01b04ba37361965a23dfeea9f2b4f236d397f50
lehuy299 <57005623+lehuy299@users.noreply.github.com>
2024-01-06 10:45:55 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { Poppins } from 'next/font/google';
+
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['600']
+})
+
+export default function Home() {
+  return (
+    <main className='h-full flex flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to bg-blue-800'>
+      <div className='space-y-6 text-center'>
+        <h1 className={cn(
+         'text-6xl text-white font-semibold drop-shadow-md',
+         font.className 
+        )}>
+          🔐Auth
+        </h1>
+        <p className='text-lg text-white'>
+          A simple authentication service
+        </p>
+        <div>
+          <Button variant='secondary' size='lg'>
+            Sign In
+          </Button>
+        </div>
+      </div>
+    </main>
+  )
+}
