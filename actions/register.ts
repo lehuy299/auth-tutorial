4f3c49cd446fa2b0fcfe6b903bc9555f405f273b
lehuy299 <57005623+lehuy299@users.noreply.github.com>
2024-01-25 00:16:35 +0700

    register final

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,35 @@
 "use server"
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
 
     if (!validatedFields.success) {
-        return { error: "Invalid fields!" }
+        return { error: "Invalid fields!" };
     }
 
-    return { success: "Email sent!" }
+    const { email, name, password } = validatedFields.data;
+
+    const existingUser = getUserByEmail(email);
+
+    if (!existingUser) {
+        return { error: "Email already in used!" };
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await db.user.create({
+        data: {
+            email,
+            name,
+            password: hashedPassword
+        }
+    });
+
+    return { success: "Email sent!" };
 };
