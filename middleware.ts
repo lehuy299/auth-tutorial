861f4e825851f3c69836b3515974a28d5a0d8c60
lehuy299 <57005623+lehuy299@users.noreply.github.com>
2024-02-03 13:11:42 +0700

    middleware 2

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,35 @@
-import { auth } from "./auth"
+import { auth } from "./auth";
+import {
+    DEFAULT_LOGIN_REDIRECT,
+    apiAuthPrefix,
+    authRoutes,
+    publicRoutes
+} from './routes'
 
 export default auth((req) => {
-    console.log("ROUTE: ", req.nextUrl.pathname);
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth;
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+    if (isApiAuthRoute) {
+        return null;
+    }
+
+    if (isAuthRoute) {
+        if (isLoggedIn) {
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+        }
+        return null;
+    }
+
+    if (!isLoggedIn && !isPublicRoute) {
+        return Response.redirect(new URL('/auth/login', nextUrl));
+    }
+
+    return null;
 })
 
 // Optionally, don't invoke Middleware on some paths
--- routes.ts
+++ routes.ts
@@ -17,3 +17,16 @@ export const authRoutes = [
     "/auth/login",
     "/auth/register"
 ];
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type {string}
+ */
+export const apiAuthPrefix = '/api/auth';
+
+/**
+ * The default redirect path after logging in
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = '/settings';
