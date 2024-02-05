513a401a0f87874016615549056891a6a304e40f
lehuy299 <57005623+lehuy299@users.noreply.github.com>
2024-02-05 18:03:15 +0700

    login final

--- auth.ts
+++ auth.ts
@@ -1,9 +1,15 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+import { db } from "@/lib/db"
+import authConfig from "./auth.config"
 
 export const {
   handlers: { GET, POST },
   auth,
+  signIn,
+  signOut
 } = NextAuth({
-  providers: [GitHub],
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
 })
