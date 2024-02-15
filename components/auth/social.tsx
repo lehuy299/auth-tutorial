46d57ded88ca1a7af4ea9ba216661ebdd4b06bd0
lehuy299 <57005623+lehuy299@users.noreply.github.com>
2024-02-15 17:00:06 +0700

    oauth provider

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -3,8 +3,15 @@
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 import { Button } from '@/components/ui/button';
+import { signIn } from 'next-auth/react';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
 const Social = () => {
+    const onClick = (provider: 'google' | 'github') => {
+        signIn(provider, {
+            callbackUrl: DEFAULT_LOGIN_REDIRECT
+        })
+    };
 
     return (
         <div className='flex items-center w-full gap-x-2'>
@@ -12,7 +19,7 @@ const Social = () => {
                 size='lg'
                 variant='outline'
                 className='w-full'
-                onClick={() => {}}
+                onClick={() => onClick('google')}
             >
                 <FcGoogle className='w-5 h-5' />
             </Button>
@@ -20,7 +27,7 @@ const Social = () => {
                 size='lg'
                 variant='outline'
                 className='w-full'
-                onClick={() => {}}
+                onClick={() => onClick('github')}
             >
                 <FaGithub className='w-5 h-5' />
             </Button>
